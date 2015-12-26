28e0cffe4bfddfa0d0132b6136d7865085a34574
chiranthhd1 <chd@stevens.edu>
2015-12-26 15:32:18 -0500

    added machine learning code

--- Decision  tree/decisionTree.py
+++ Decision  tree/decisionTree.py
@@ -1,20 +1,21 @@
 from sklearn import tree
 
+
 data = [] # training data set
 labels = [] # traning label set
 test = [] # test data set
 
-with open ('uptest1.csv') as testf:
+with open ('uptest.csv') as testdata:
     row = 0 # skip the first line
-    for line in testf:
+    for line in testdata:
         testSamples = line.strip().split(",") # trim whitespaces
         if(row >0):
             test.append([float(num) for num in testSamples]) # convert string to float
         row +=1
 
-with open ('uptraining.csv') as f:
+with open ('uptrain.csv') as traindata:
     row = 0 # skip the first line
-    for line in f:
+    for line in traindata:
         trainSamples = line.strip().split(",") # trim whitespaces
         if(row > 0):
             data.append([float(num) for num in trainSamples[:-1]])
@@ -23,5 +24,5 @@ with open ('uptraining.csv') as f:
 
 clf = tree.DecisionTreeClassifier(criterion='entropy')
 clf = clf.fit(data, labels)
-
 print(clf.predict(test))
+
